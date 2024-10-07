75f28f371bef998b0d2eb3abaedf17a97457c4e0
T34 <t34.dev@gmail.com>
2024-10-07 12:38:29 +0300

    changed the names

--- pkg/sys/error.go
+++ pkg/sys/error.go
@@ -10,7 +10,7 @@ type commonError struct {
 	code codes.Code
 }
 
-func NewCommonError(msg string, code codes.Code) *commonError {
+func NewError(msg string, code codes.Code) *commonError {
 	return &commonError{msg, code}
 }
 
@@ -22,12 +22,12 @@ func (r *commonError) Code() codes.Code {
 	return r.code
 }
 
-func IsCommonError(err error) bool {
+func IsError(err error) bool {
 	var ce *commonError
 	return errors.As(err, &ce)
 }
 
-func GetCommonError(err error) *commonError {
+func GetError(err error) *commonError {
 	var ce *commonError
 	if !errors.As(err, &ce) {
 		return nil
